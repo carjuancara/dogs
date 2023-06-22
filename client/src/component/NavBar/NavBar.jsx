5cc2eeea300ef2e2619f5bff6f544a4bcdeb621e
carlos carabajal <carjuancara@gmail.com>
2023-06-22 14:49:57 -0300

    FEAT/UPDATE modify NavBar component and styles

--- client/src/component/NavBar/NavBar.jsx
+++ client/src/component/NavBar/NavBar.jsx
@@ -1,60 +1,60 @@
 import React from "react";
-import s from './NavBar.module.css';
-import { Link } from 'react-router-dom';
+import s from "./NavBar.module.css";
+import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import image from '../../image/logo.png';
+import image from "../../image/icono.jpg";
 import { getRazaByName } from "../../redux/actions.js";
 
 export default function NavBar() {
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
 
-  const [name, setName] = useState('')
-  const dispatch = useDispatch()
-  
   const handleSubmit = (e) => {
     e.preventDefault();
-      
-    dispatch(getRazaByName(name));      
-  }
-  
 
-  
-    return (
-        <div className={s.container}>
+    dispatch(getRazaByName(name));
+  };
+
+  return (
+    <div className={s.container}>
       <nav className={s.navbar}>
         <div className={s.divImg}>
           <img className={s.image} src={image} alt="" />
         </div>
         <div className={s.divItem}>
-          <Link className={s.linkNav} to='/home'>
-            <li className={s.item} >Home</li>
+          <Link className={s.linkNav} to="/home">
+            <li className={s.item}>Home</li>
           </Link>
-          <Link className={s.linkNav} to='/newdog'>
-            <li className={s.item} >New Dog</li>
+          <Link className={s.linkNav} to="/newdog">
+            <li className={s.item}>New Raza</li>
+          </Link>
+          <Link className={s.linkNav} to="/atribuciones">
+            <li className={s.item}>Atribuciones</li>
           </Link>
         </div>
-        <form className={s.divInput} onSubmit={handleSubmit} >
+        <form className={s.divInput} onSubmit={handleSubmit}>
           <input
             className={s.inputNavbar}
             value={name}
-            name='search'
+            name="search"
             autoFocus
             placeholder="Search Raza"
             type="text"
-            onChange={(e) => { setName(e.target.value) }}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
             autoComplete="off"
           />
 
-          <input className={s.inputButton} type="button" value="Search" onClick={handleSubmit} />
+          <input
+            className={s.inputButton}
+            type="button"
+            value="Search"
+            onClick={handleSubmit}
+          />
         </form>
       </nav>
-
     </div>
-    )
+  );
 }
-
-
-
-
-
-
