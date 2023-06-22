1fb47aae773a6ac3dd4482a08f7f1ecd24a68175
carlos carabajal <carjuancara@gmail.com>
2023-06-22 14:45:22 -0300

    FEAT/UPDATE component CARD

--- client/src/component/Card/Card.jsx
+++ client/src/component/Card/Card.jsx
@@ -1,25 +1,24 @@
 import React from "react";
-import s from './Card.module.css'
-import { Link } from 'react-router-dom'
-
+import s from "./Card.module.css";
+import { Link } from "react-router-dom";
 
 export default function Card({ id, name, image, weight, temperament }) {
   return (
     <Link className={s.links} to={`/dogdetail/${id}`}>
-      <div key={id} className={s.tarjetaContenedor}  >
+      <div key={id} className={s.tarjetaContenedor}>
         <div className={s.titulo}>
-           <h4 >Raza: {name}</h4>
+          <h4>RAZA: {name}</h4>
+          <h4>WEIGHT: {weight}</h4>
         </div>
-        <div className={s.titulo}>
-          <h4 >Weight: {weight}</h4>
-        </div>
+        {/* <div className={s.titulo}>
+        </div> */}
         <div className={s.containerImage}>
           <img src={image} className={s.image} alt="" />
         </div>
         <div className={s.pieTarjeta}>
-          <h4>temperaments: {temperament}</h4>
+          <h4>TEMPERAMENTS: {temperament}</h4>
         </div>
       </div>
     </Link>
-  )
+  );
 }
