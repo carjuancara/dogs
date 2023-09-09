82fe6cf8a540336fa8fa6ddc0221926b76bae157
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-09-09 13:05:13 -0300

    FEAT actualize vista de datos de la tarjeta

--- client/src/component/Card/Card.jsx
+++ client/src/component/Card/Card.jsx
@@ -2,21 +2,23 @@ import React from "react";
 import s from "./Card.module.css";
 import { Link } from "react-router-dom";
 
-export default function Card({ id, name, image, weight, temperament }) {
+export default function Card({ id, name, image, temperament }) {
+
   return (
     <Link className={s.links} to={`/dogdetail/${id}`}>
       <div key={id} className={s.tarjetaContenedor}>
         <div className={s.titulo}>
           <h4>RAZA: {name}</h4>
-          <h4>WEIGHT: {weight}</h4>
         </div>
-        {/* <div className={s.titulo}>
-        </div> */}
         <div className={s.containerImage}>
           <img src={image} className={s.image} alt="" />
         </div>
         <div className={s.pieTarjeta}>
-          <h4>TEMPERAMENTS: {temperament}</h4>
+        {temperament ?
+          <h4>TEMPERAMENTS: {temperament.length<20 ? temperament : temperament.slice(0,20)+"..."}</h4>
+          :
+          null
+        }
         </div>
       </div>
     </Link>
