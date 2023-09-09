10d7e47ed44a21f52f3ec54309ac1b38367b287e
Carlos Juan Carabajal <carjuancara@gmail.com>
2023-09-09 12:52:10 -0300

    FEAT / ADD ruta de actualizacion de raza

--- api/src/routes/dogRouter.js
+++ api/src/routes/dogRouter.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const dogRouter = Router();
-const { handleDogsAll, handleDogById, handleDogCreate } = require('../handlers/dogHandler')
+const { handleDogsAll, handleDogById, handleDogCreate, handleDogUpdate } = require('../handlers/dogHandler')
 
 
 dogRouter.get('/', handleDogsAll)
 dogRouter.get('/:idRaza', handleDogById)
 dogRouter.post('/', handleDogCreate)
+dogRouter.put('/', handleDogUpdate)
 
 module.exports = dogRouter
