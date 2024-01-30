c2f16d4945620afc35359365a997d98be8325758
Carlos Juan Carabajal <carjuancara@gmail.com>
2024-01-30 17:07:29 -0300

    feat: :fire: quito todo el codigo para manejar los detalles de una raza
    la lógica para traer los detalles de una raza se movieron a el componente dogDetail

--- client/src/store/dogStore-origin.ts
+++ client/src/store/dogStore-origin.ts
@@ -16,20 +16,10 @@ interface responseDog {
   temperament: string;
   year: string;
 }
-interface details {
-  image: string;
-  name: string;
-  ApiID?: number;
-  id?: string;
-  temperament: string;
-  weight: string;
-  height: string;
-  year: string;
-}
+
 interface useDog {
   allDog: responseDog[];
   backupDog: responseDog[];
-  dogDetail: details;
   currentPage: number;
   temperaments: objectTemperament[];
   temperamentByRaza: object[];
@@ -39,9 +29,7 @@ interface useDog {
   filterOrigin: string;
   UpdateCurrentPage: (value: number) => void;
   GoToPage: (value: number) => void;
-  cleanDogDetail: () => void;
   searchTemperaments: (temp: []) => void;
-  DogDetail: (origin: string, id: number) => void;
   restoreAllDogs: () => void;
   filterByOrigin: (origin: string) => void;
   filterByTemperaments: (value: string) => void;
@@ -81,29 +69,6 @@ export const useDogStore = create<useDog>()((set) => ({
       currentPage: value
     }))
   },
-  cleanDogDetail: () => {
-    set((state) => ({
-      ...state,
-      dogDetail: {
-        image: "",
-        name: "",
-        ApiID: 0,
-        id: "",
-        temperament: "",
-        weight: "",
-        height: "",
-        year: ""
-      }
-    }))
-  },
-  /* searchTemperaments: (temp: []) => set( state => {
-    temp.forEach(r => {
-      const found = state.temperaments.find((t) => t.name === r)
-      if (found) {
-        state.temperamentByRaza.push({ id: found.id, name: found.name })
-      }
-    })
-}), */
   searchTemperaments: (temp: string[]) => set((state) => {
     const newTemperamentByRaza = [...state.temperamentByRaza]; // Copia el estado existente
 
@@ -119,28 +84,6 @@ export const useDogStore = create<useDog>()((set) => ({
       temperamentByRaza: newTemperamentByRaza, // Actualiza la propiedad temperamentByRaza
     };
   }),
-  /* DogDetail: (origen: string, id: number) => {
-    set((state) => ({
-      ...state,
-      dogDetail: origen === 'ApiID'
-        ? state.allDog.find( dog => dog.ApiID === id)
-        : state.allDog.find( dog => dog.id === id)
-    }))
-  }, */
-  DogDetail: (origen: string, id: number) => {
-    set((state: useDog) => ({
-      ...state,
-      dogDetail: {
-        ...state.dogDetail, // Mantén los datos existentes en dogDetail
-        ...(
-          origen === 'ApiID'
-            ? state.allDog.find(dog => dog.ApiID === id)
-            : state.allDog.find(dog => dog.id === id.toString())
-        )
-      }
-    }))
-  },
-  
   restoreAllDogs: () => set((state: useDog) => ({
     ...state,
     allDog: state.backupDog
@@ -195,27 +138,6 @@ export const useDogStore = create<useDog>()((set) => ({
       allDog: sortedAllDog,
     };
   }),
-  /* sortByWeight: (value: string | number) => set((state) => {
-    let sortOrder: number;
-    if (typeof value === "string") {
-      sortOrder = value === 'ASC' ? 1 : -1;
-    } else if (typeof value === "number") {
-      sortOrder = value; // Si ya es un número, úsalo directamente
-    } else {
-      sortOrder = 1; // Valor predeterminado si el tipo no es reconocido
-    }
-  
-    const clonedAllDog = [...state.allDog];
-    const sortedAllDog = clonedAllDog.sort((a, b) => {
-      return a.weight.localeCompare(b.weight) * sortOrder;
-    });
-  
-    return {
-      ...state,
-      currentPage: 1,
-      allDog: sortedAllDog,
-    };
-  }), */
   getAllDogs: async () => {
     try {
       const response = await axios.get("/dogs");
@@ -241,7 +163,7 @@ export const useDogStore = create<useDog>()((set) => ({
     set({ temperaments: temperaments.Temperaments })
   },
   getRazaByName: async (nameRaza: string) => {
-    const allDog = await (await axios(`/dogs`, { params: { name: nameRaza } })).data
+    const allDog = await (await axios(`http://localhost:3001/dogs`, { params: { name: nameRaza } })).data
     set((state: useDog) => ({
       ...state,
       currentPage: 1,
