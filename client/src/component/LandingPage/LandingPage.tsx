4dc5d5eb602cfc70fe0c29b7fd787e607319c0f4
Carlos Juan Carabajal <carjuancara@gmail.com>
2024-01-30 08:30:32 -0300

    feat: :lipstick: actualize estilos en la landing page

--- client/src/component/LandingPage/LandingPage.tsx
+++ client/src/component/LandingPage/LandingPage.tsx
@@ -1,19 +1,30 @@
 import React from "react";
-import image from "../../image/background.jpg";
-import { Link } from "react-router-dom";
 
-export default function LandingPage() {
+import { Link } from "react-router-dom"
+import image from '../../image/background.jpg'
+
+export default function LandingPage () {
+
   return (
-    <div className='container flex items-center justify-center w-screen h-screen'>
-      <img className="w-screen h-screen absolute bg-no-repeat bg-cover bg-center" src={image} alt="" />
-      <div className="flex flex-col mr-80 items-center relative border-2 border-solid border-antiquewhite rounded-lg h-36 w-72 justify-evenly">
-        <h2 className="text-xl font-bold text-white" >Dog APP</h2>
-        <button className="w-48 h-12 rounded-md bg-gray-500 text-white font-bold text-2xl cursor-pointer">
+    <div className="flex h-screen text-white">
+      <div className="w-1/3 bg-gray-800 flex items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold">The Dog Api</h1>
+          <p>Descubre todo lo que tenemos para ofrecer.</p>
           <Link className="no-underline" to="/home">
-            Enter Site
+            <button className="bg-yellow-500 text-white px-6 py-2 mt-4 hover:bg-yellow-600 rounded-lg font-bold">Ingresar</button>
           </Link>
-        </button>
+
+        </div>
+      </div>
+      <div className="w-2/3 relative overflow-hidden">
+        <img
+          src={image}
+          alt="Landing"
+          className={`w-full h-full object-cover`}
+        />
+
       </div>
     </div>
-  );
+  )
 }
